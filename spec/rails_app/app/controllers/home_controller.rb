b829a6621f67004589fc00fb2dca294896f8f290
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-11-04 12:48:37 +0100

    Use https where possible
    Update "Crafting Rails 4 Applications" link. The old book is not
    available anymore and the redirect results in a page not found error.
    
    In the source files of "Crafting Rails 4 Applications", available at
    https://pragprog.com/titles/jvrails2/crafting-rails-4-applications/,
    a `HomeController` file is available found with an updated copyright
    notice.

--- spec/rails_app/app/controllers/home_controller.rb
+++ spec/rails_app/app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 #---
-# Excerpted from "Crafting Rails Applications",
+# Excerpted from "Crafting Rails 4 Applications",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/jvrails for more book information.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/jvrails2/crafting-rails-4-applications/ for more book information.
 #---
 class HomeController < ApplicationController
   def index
